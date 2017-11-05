ae56882dcde5efb7eb5c71646315ff21b4a3e983
Argi Avetisyan <argishtiavetis@gmail.com>
2017-11-04 19:17:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser('S3CRE7'));
 app.use(session({
-    secret: 'erazimerkirhayrenihogserdshathuysdmetsg',
+    secret: 'erazimerkirhayrenihogserdshathuysdmetsgh',
     name: 'none-of-your-business-buahaha',
     proxy: true,
     resave: true,
